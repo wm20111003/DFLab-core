b34313b085295c07997843bc1d1501203c2f4a28
王明明 <wangmingming@datafountain.cn>
2020-06-07 14:47:41 +0800

    auth login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, Body } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
-@Controller('auth')
-export class AuthController {}
+@Controller("auth")
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post("/login")
+  async getUsers(@Req() req, @Body() body): Promise<any> {
+    console.log("params", body);
+    return await this.authService.toLogin(body);
+  }
+}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+
+interface LoginInfo {
+  username: string;
+  password: string;
+}
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async toLogin(body: LoginInfo) {
+    if (body.username === "admin") {
+      return {
+        status: 200,
+        message: "登录成功",
+      };
+    } else {
+      return {
+        status: 10010,
+        message: "登录失败,用户名密码错误",
+      };
+    }
+  }
+}
